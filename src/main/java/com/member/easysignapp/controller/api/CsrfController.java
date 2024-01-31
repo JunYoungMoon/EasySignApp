cadd04a771a3b1ca37ec49274a29c06cd9b4145e
dev2 <ko5642027@naver.com>
2024-01-31 11:53:34 +0900

    다국어 처리 완료

--- src/main/java/com/member/easysignapp/controller/api/CsrfController.java
+++ src/main/java/com/member/easysignapp/controller/api/CsrfController.java
@@ -1,6 +1,8 @@
 package com.member.easysignapp.controller.api;
 
 import com.member.easysignapp.dto.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,21 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("/api")
 public class CsrfController {
+    private final MessageSourceAccessor messageSourceAccessor;
+
+    @Autowired
+    public CsrfController(MessageSourceAccessor messageSourceAccessor) {
+        this.messageSourceAccessor = messageSourceAccessor;
+    }
     @GetMapping("/getcsrf")
     public ApiResponse getCsrfToken(HttpServletRequest servletRequest) {
+
+        String successMessage = messageSourceAccessor.getMessage("success.message.key");
+
         return ApiResponse.builder()
                 .status("success")
                 .csrfToken(((CsrfToken) servletRequest.getAttribute(CsrfToken.class.getName())).getToken())
-                .msg("Success message")
+                .msg(successMessage)
                 .build();
     }
 }
