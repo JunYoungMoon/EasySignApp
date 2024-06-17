f7138ba7d1494037ffecdfed1a9fb7ac28b8b042
dev2 <ko5642027@naver.com>
2024-06-17 16:07:35 +0900

    자동빌드 오류 문구 때문에 OpenAPI 버전업

--- src/main/java/com/member/easysignapp/config/OpenAPIConfig.java
+++ src/main/java/com/member/easysignapp/config/OpenAPIConfig.java
@@ -26,7 +26,7 @@ import org.springframework.context.annotation.Configuration;
 //}
 
 @Configuration
-public class SwaggerConfig {
+public class OpenAPIConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
